a3908cd0f29cd4e51149ca1ba2b31bd352344306
nelyivanova-maker <nelyivanova@gmail.com>
2026-05-04 07:43:58 +0100

    Create CreateVideo.tsx

---
+++ app/components/CreateVideo.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function CreateVideo() {
+  return (
+    <section style={{ flex: 1, background: "#111827", padding: 24, borderRadius: 24, color: "#fff" }}>
+      <h2>Create Video</h2>
+
+      <p>Video section will be developed next.</p>
+    </section>
+  );
+}
